bb055eb4617c7ebbeb537cfddf40ceb657be0f52
meriemf <farsi.meriem60@gmail.com>
2020-11-14 19:28:41 -0500

    enable ssl in index.js

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ console.log("pass", process.env.DB_PASS)
 console.log(`Connected to ${process.env.DB_NAME} on ${process.env.DB_HOST}`);
 
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=require`;
 
 const client = new pg.Client({
   connectionString: connectionString || process.env.DATABASE_URL,
